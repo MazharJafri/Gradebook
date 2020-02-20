e172fa03a5a06383e96eaeaf753930bb318c6d87
Mazhar Jafri <mohammedjafri94@gmail.com>
2020-02-20 16:09:33 -0600

    Encapsuled Statistic Methods

--- Program.cs
+++ Program.cs
@@ -1,31 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using Gradebook;
 
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
-        {          
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);            
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result += number;
-            } 
-            Console.WriteLine(result);
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else 
-            {
-                Console.WriteLine("Hello!");
-            }
+        {   
+            var book = new Book("Mazhar's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
         }        
     }
 }
