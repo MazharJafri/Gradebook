860707bfeb805dc97da100ef1585600c85922f06
Mazhar Jafri <mohammedjafri94@gmail.com>
2020-02-22 15:22:48 -0600

    Ref. Types vs. Value Types Unit Tests

--- src/test/GradeBook.Tests/BookTests.cs
+++ src/test/GradeBook.Tests/BookTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class Booktests
+    public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAnAverageGrade()
         {
             // Arrange
             var book = new Book("");
