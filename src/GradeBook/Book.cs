997be4df074ee9f1a4cf43503bf01e0640310af7
Mazhar Jafri <mohammedjafri94@gmail.com>
2020-02-24 17:31:49 -0600

    Properties and Delegates

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+    
     public class Book
     {
         public Book(string name)
@@ -11,29 +13,142 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddLetterGrade(string letter)
+        {
+            switch(letter)
+            {
+                case "A+":
+                    AddGrade(98);
+                    break;
+                case "A":
+                    AddGrade(95);
+                    break;
+                case "A-":
+                    AddGrade(90);
+                    break;
+                case "B+":
+                    AddGrade(88);
+                    break;
+                case "B":
+                    AddGrade(85);
+                    break;
+                case "B-":
+                    AddGrade(80);
+                    break;
+                case "C+":
+                    AddGrade(78);
+                    break;
+                case "C":
+                    AddGrade(75);
+                    break;
+                case "C-":
+                    AddGrade(70);
+                    break;
+                case "D+":
+                    AddGrade(68);
+                    break;
+                case "D":
+                    AddGrade(65);
+                    break;
+                case "D-":
+                    AddGrade(60);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
          public void AddGrade(double grade) 
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else 
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
+
+        public event GradeAddedDelegate GradeAdded;
 
         public Statistics GetStatistics()
         {
             var result = new Statistics();
+            result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach(var grade in grades)
+            for (var index = 0; index < grades.Count; index++)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
-            } 
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+            }
+
             result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 98.0:
+                    result.Letter = "A+";
+                    break;
+                case var d when d >= 95.0:
+                    result.Letter = "A";
+                    break;
+                case var d when d >= 90.0:
+                    result.Letter = "A-";
+                    break;
+                case var d when d >= 88.0:
+                    result.Letter = "B+";
+                    break;
+                case var d when d >= 85.0:
+                    result.Letter = "B";
+                    break;             
+                case var d when d >= 80.0:
+                    result.Letter = "B-";
+                    break;   
+                case var d when d >= 78.0:
+                    result.Letter = "C+";
+                    break;
+                case var d when d >= 75.0:
+                    result.Letter = "C";
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = "C-";
+                    break;
+                case var d when d >= 68.0:
+                    result.Letter = "D+";
+                    break;
+                case var d when d >= 65.0:
+                    result.Letter = "D";
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = "D-";
+                    break;
+
+                default:
+                    result.Letter = "F";
+                    break;
+            }
             
             return result;
         }
 
         private List<double> grades;
-        public string Name;
+        
+        public string Name
+        {
+            get; 
+            set;
+        }
+
+        public const string CATEGORY = "Science";
     }
 }
